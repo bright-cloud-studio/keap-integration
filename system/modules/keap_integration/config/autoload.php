7840e03d630f8ea18e18506be9c5b3d1e54708c5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-18 13:58:20 -0400

    Create autoload.php

---
+++ system/modules/keap_integration/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Keap Integration
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/keap_integration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+	'KeapIntegration\Handler' => 'system/modules/keap_integration/src/Handler.php'
+));
