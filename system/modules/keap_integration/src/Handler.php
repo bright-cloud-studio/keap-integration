6b3365c41c61b0dbaffae747841fcf68fb7c71df
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-19 15:17:56 -0400

    Update Handler.php

--- system/modules/keap_integration/src/Handler.php
+++ system/modules/keap_integration/src/Handler.php
@@ -42,8 +42,6 @@ class Handler
         self::$arrUserOptions       = $arrData;
         self::$arrUserOptions['id'] = $intId;
 
-        echo var_dump($arrData);
-
         $infusionsoft = new \Infusionsoft\Infusionsoft(array(
             'clientId' => 'IoAJ9zFbZnszZHWkTxp7vFj5zg0TII2g',
             'clientSecret' => 'xS3oRvWe5kgcXjdG',
@@ -132,95 +130,13 @@ class Handler
     
     
     
-    public function accountActivated($intId, $arrData, $objModule)
+    public function accountActivated($member, $module)
     {
-        // Store the registration data.
-        self::$arrUserOptions       = $arrData;
-        self::$arrUserOptions['id'] = $intId;
-
-        echo var_dump($arrData);
-
-        $infusionsoft = new \Infusionsoft\Infusionsoft(array(
-            'clientId' => 'IoAJ9zFbZnszZHWkTxp7vFj5zg0TII2g',
-            'clientSecret' => 'xS3oRvWe5kgcXjdG',
-            'redirectUri' => 'https://framework.brightcloudstudioserver.com/registration-success.html',
-        ));
-
-        // If the serialized token is available in the session storage, we tell the SDK
-        // to use that token for subsequent requests.
-        if (isset($_SESSION['token'])) {
-            $infusionsoft->setToken(unserialize($_SESSION['token']));
-        }
-
-        // If we are returning from Infusionsoft we need to exchange the code for an
-        // access token.
-        if (isset($_GET['code']) and !$infusionsoft->getToken()) {
-            $infusionsoft->requestAccessToken($_GET['code']);
-
-            // Save the serialized token to the current session for subsequent requests
-            $_SESSION['token'] = serialize($infusionsoft->getToken());
-        }
-
-        function add($infusionsoft, $email, $arrData)
-        {
-            // build out the json data to send to Keap
-
-            // DATA - Contact Type
-            $contact_type = 'Website Lead - EFS Myths';
-
-             // DATA - Email
-            $email1 = new \stdClass;
-            $email1->field = 'EMAIL1';
-            $email1->email = $email;
-
-            // DATA - Family Name
-            $family_name =  $arrData['lastname'];
-
-            // DATA - Given Name
-            $given_name =  $arrData['firstname'];
-
-            // DATA - Lead Source ID
-            $lead_source_id = '19';
-
-            // Entire Contact
-            //$contact = ['given_name' => $given_name, 'family_name' => $family_name, 'email_addresses' => [$email1], 'contact_type' => $contact_type, 'lead_source_id' => $lead_source_id];
-            $contact = ['given_name' => $given_name, 'family_name' => $family_name, 'email_addresses' => [$email1]];
-
-            return $infusionsoft->contacts()->create($contact);
-        }
-
-        if ($infusionsoft->getToken()) {
-            try {
-
-                //$email = 'john.doe4@example.com';
-                $email = $arrData['email'];
-
-                try {
-                    $cid = $infusionsoft->contacts()->where('email', $email)->first();
-                } catch (\Infusionsoft\InfusionsoftException $e) {
-                    $cid = add($infusionsoft, $email, $arrData);
-                }
-
-            } catch (\Infusionsoft\TokenExpiredException $e) {
-                // If the request fails due to an expired access token, we can refresh
-                // the token and then do the request again.
-                $infusionsoft->refreshAccessToken();
-
-                $cid = add($infusionsoft);
-            }
-
-            $contact = $infusionsoft->contacts()->with('custom_fields')->find($cid->id);
-
-            var_dump($contact->toArray());
-
-            // Save the serialized token to the current session for subsequent requests
-            $_SESSION['token'] = serialize($infusionsoft->getToken());
-        } else {
-            echo '<a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to authorize</a>';
-        }
-
+        // Do Stuff
+        
+        
         // Testing the controller log
-        \Controller::log('Keap Integration: ' . $email . ' sent to Keap using API',
+        \Controller::log('Keap Integration: Member Account Activated',
             __CLASS__ . '::' . __FUNCTION__,
             'GENERAL'
         );
