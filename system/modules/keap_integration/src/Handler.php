bb3271c96754649c484a6e50e96f7bcfc15d3a0b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-20 11:50:39 -0400

    Update Handler.php

--- system/modules/keap_integration/src/Handler.php
+++ system/modules/keap_integration/src/Handler.php
@@ -38,9 +38,8 @@ class Handler
      */
     public function newUserCreated($intId, $arrData, $objModule)
     {
-        echo "ID: " . $objModule->arrData->id;
-        echo $objModule;
-        echo $objModule->arrData;
+        echo "ID: " . $objModule['arrData']['id'];
+        echo $objModule['arrData'];
         die();
         
         // Store the registration data.
