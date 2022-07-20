681b3e62e92bc3140d1d0fb77bd009747283366e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-20 10:50:03 -0400

    Update tl_module.php

--- system/modules/keap_integration/dca/tl_module.php
+++ system/modules/keap_integration/dca/tl_module.php
@@ -2,18 +2,38 @@
 
 // Modify registration palette
 $GLOBALS['TL_DCA']['tl_module']['palettes']['registration'] = str_replace(
-    'disableCaptcha;',
-    'disableCaptcha;{fieldDependencySettings_legend},mcsUseJavaScript;',
+    'tableless;',
+    'tableless;{keapIntegration_legend},keapCliendID,keapClientSecret,keapRedirectUrl;',
     $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']
 );
 
 // Add fields
-$GLOBALS['TL_DCA']['tl_module']['fields']['mcsUseJavaScript'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_module']['mcsUseJavaScript'],
-    'exclude' => true,
-    'inputType' => 'checkbox',
-    'eval' => [
-        'submitOnChange' => true
-    ],
-    'sql' => "char(1) NOT NULL default '1'",
+$GLOBALS['TL_DCA']['tl_module']['fields']['keapCliendID'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['keapCliendID'],
+    'inputType'               => 'text',
+    'default'		          => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 ];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['keapClientSecret'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['keapClientSecret'],
+    'inputType'               => 'text',
+    'default'		          => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['keapRedirectUrl'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['keapRedirectUrl'],
+    'inputType'               => 'text',
+    'default'		          => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
+];
+
+
+
