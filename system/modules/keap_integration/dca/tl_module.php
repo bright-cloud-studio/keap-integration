9813ca50bed9f3797ee0a96ccc5103b681047b5b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-20 11:06:37 -0400

    Update tl_module.php

--- system/modules/keap_integration/dca/tl_module.php
+++ system/modules/keap_integration/dca/tl_module.php
@@ -28,12 +28,11 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['keapClientSecret'] = [
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['keapRedirectUrl'] = [
     'label'                   => &$GLOBALS['TL_LANG']['tl_module']['keapRedirectUrl'],
-    'exclude'                 => true,
-    'inputType'               => 'pageTree',
-    'foreignKey'              => 'tl_page.title',
-    'eval'                    => array('fieldType'=>'radio'),
-    'sql'                     => "int(10) unsigned NOT NULL default ''",
-    'relation'                => array('type'=>'hasOne', 'load'=>'eager')
+    'inputType'               => 'text',
+    'default'		          => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 ];
 
 
