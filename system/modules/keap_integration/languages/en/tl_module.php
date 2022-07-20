568ece581ce876fd2acae136e18ecffc520d4d53
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-20 12:51:58 -0400

    Update tl_module.php

--- system/modules/keap_integration/languages/en/tl_module.php
+++ system/modules/keap_integration/languages/en/tl_module.php
@@ -18,3 +18,5 @@ $GLOBALS['TL_LANG']['tl_module']['keapIntegration_legend'] = 'Keap Integration S
 $GLOBALS['TL_LANG']['tl_module']['keapCliendID'] = ['API Client ID', 'Enter the Keap app\'s Client ID.'];
 $GLOBALS['TL_LANG']['tl_module']['keapClientSecret'] = ['API Client Secret', 'Enter the Keap app\'s Client Secret.'];
 $GLOBALS['TL_LANG']['tl_module']['keapRedirectUrl'] = ['API Redirect URL', 'Enter the URL to be redirected to after successfully passing data to Keap.'];
+
+$GLOBALS['TL_LANG']['tl_module']['keapSecurityToken'] = ['Keap Security Token', 'Hidden field for storing the security token to gain access to Keap.'];
