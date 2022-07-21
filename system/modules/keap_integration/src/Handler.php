49b85a87a957a9cde6748352d653eda0580510b0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-07-21 12:49:57 -0400

    Update Handler.php

--- system/modules/keap_integration/src/Handler.php
+++ system/modules/keap_integration/src/Handler.php
@@ -38,6 +38,7 @@ class Handler
      */
     public function newUserCreated($intId, $arrData, $objModule)
     {
+        $objModule->set('keapSecurityToken','test123');
         
         // Store the registration data.
         self::$arrUserOptions       = $arrData;
