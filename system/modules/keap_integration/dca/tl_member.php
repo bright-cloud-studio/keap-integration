cd22c457efdf1d31e76e4fbe5be2178ab3a9c808
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-08-16 10:11:07 -0400

    Create tl_member.php

---
+++ system/modules/keap_integration/dca/tl_member.php
@@ -0,0 +1,12 @@
+<?
+
+
+/* Add fields to tl_user */
+$GLOBALS['TL_DCA']['tl_member']['fields']['state'] = array
+(
+  'sorting'                 => true,
+  'inputType'               => 'select',
+  'options'                 => array('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'),
+  'eval'                    => array('includeBlankOption'=>true, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'address', 'tl_class'=>'w50'),
+  'sql'                     => "varchar(64) NOT NULL default ''"
+);
