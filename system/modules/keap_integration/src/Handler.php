0691df563882847a64fb0e9a0057198d2810cd3d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-20 11:47:43 -0400

    Update Handler.php

--- system/modules/keap_integration/src/Handler.php
+++ system/modules/keap_integration/src/Handler.php
@@ -57,29 +57,31 @@ class Handler
             // DATA - Contact Type
             $contact_type =  'Prospect';
             
-            $addr = new \stdClass;
-            $addr->field = 'BILLING';
-            $addr->line1 = $arrData['street'];
-            $addr->region = $arrData['state'];
-            $addr->line2 = "";
-            $addr->locality = $arrData['city'];
-            $addr->postal_code = $arrData['postal'];
-            $addr->zip_code = $arrData['postal'];
-            $addr->zip_four = "";
-            $addr->country_code = "USA";
+            $opt_in_reason = 'Contact gave explicit permission.';
+            
+            //$addr = new \stdClass;
+            //$addr->field = 'BILLING';
+            //$addr->line1 = $arrData['street'];
+            //$addr->region = $arrData['state'];
+            //$addr->line2 = "";
+            //$addr->locality = $arrData['city'];
+            //$addr->postal_code = $arrData['postal'];
+            //$addr->zip_code = $arrData['postal'];
+            //$addr->zip_four = "";
+            //$addr->country_code = "USA";
             
             
-            $phone = new \stdClass;
-            $phone->number = $arrData['phone'];
-            $phone->extension = "";
-            $phone->field = 'PHONE1';
-            $phone->type = 'Work';
+            //$phone = new \stdClass;
+            //$phone->number = $arrData['phone'];
+            //$phone->extension = "";
+            //$phone->field = 'PHONE1';
+            //$phone->type = 'Work';
             
             
 
             // Entire Contact
-            $contact = ['contact_type' => $contact_type, 'lead_source_id' => $lead_source_id, 'given_name' => $given_name, 'family_name' => $family_name, 'email_addresses' => [$email1], 'addresses' => [$addr], 'phone_numbers' => [$phone]];
-            //$contact = ['given_name' => $given_name, 'family_name' => $family_name, 'email_addresses' => [$email1]];
+            //$contact = ['contact_type' => $contact_type, 'lead_source_id' => $lead_source_id, 'given_name' => $given_name, 'family_name' => $family_name, 'email_addresses' => [$email1], 'addresses' => [$addr], 'phone_numbers' => [$phone]];
+            $contact = ['given_name' => $given_name, 'family_name' => $family_name, 'opt_in_reason' => $opt_in_reason, 'lead_source_id' => $lead_source_id, 'email_addresses' => [$email1]];
 
             return $infusionsoft->contacts()->create($contact);
         }
@@ -101,13 +103,18 @@ class Handler
 
             //var_dump($contact->toArray());
 
-            // Add our user to the requested campaign
+            // Myths of Money - EFS - ID: 57
             $integration = 'suz811';
-            $callName = 'MythsAPICall';
+            $callName = 'MythsEFS';
             $infusionsoft->funnels()->achieveGoal($integration, $callName, $cid->id);
+
+            // Add our user to the requested campaign
+            //$integration = 'suz811';
+            //$callName = 'MythsAPICall';
+            //$infusionsoft->funnels()->achieveGoal($integration, $callName, $cid->id);
             
-            // Add custom tag: YOD Live Stream June 2022
-            $tag_ids = [165,127];
+            // Add custom tag: MythsofMoneyEpsteinFS
+            $tag_ids = [127];
             $infusionsoft->contacts()->find($cid->id)->addTags($tag_ids);
             
             
